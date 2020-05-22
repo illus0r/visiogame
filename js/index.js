7767612d15284776f8507a454f40cbe98db36958
illus0r <zor667@gmail.com>
2020-05-22 14:57:09 +0400

    traking correctness of answers, showing result. Different answers for true and wrong answers

--- js/index.js
+++ js/index.js
@@ -1,6 +1,23 @@
 function Game() {
   this.currentQuestion = 0
   this.answerIsShown = false
+	this.isLastAnswerCorrect = true
+	this.questionCount = document.querySelectorAll("#game .optionSet").length
+	this.questionsCorrectness = []
+
+	this.countCorrect = () => {
+		let sum = 0
+		for (let q of this.questionsCorrectness)
+			sum += q
+		return sum
+	}
+
+	this.showResult = () => {
+		const result = document.querySelector("#result")
+		result.querySelector(".correct").innerText = this.countCorrect()
+		result.querySelector(".total").innerText = this.questionCount
+		result.classList.add('visible')
+	}
 
   this.shuffleAnswers = () => {
     const optionSets = document
@@ -21,19 +38,20 @@ function Game() {
       questions[i].classList.remove('visible')
     }
       
+		if (this.currentQuestion >= this.questionCount) {
+			this.showResult()
+			return
+		}
+
     const currentQuestion = document
       .querySelector(`#game .question-${ this.currentQuestion }`)
 
     currentQuestion.classList.add('visible')
 
-    if (this.answerIsShown) {
-      currentQuestion.querySelector('#game .answer')
-        .classList.add('visible')
-    }
-    else {
-      currentQuestion.querySelector('#game .answer')
-        .classList.remove('visible')
-    }
+    currentQuestion.querySelector('#game .answer')
+      .classList.toggle('visible', this.answerIsShown)
+    currentQuestion.querySelector('#game .answer .correct').classList.toggle('visible', this.isLastAnswerCorrect)
+    currentQuestion.querySelector('#game .answer .wrong').classList.toggle('visible', !this.isLastAnswerCorrect)
   }
 
   this.showNextQuestion = () => {
@@ -42,8 +60,10 @@ function Game() {
     this.updateView()
   }
 
-  this.showAnswer = () => {
+  this.showAnswer = (correctness) => {
     this.answerIsShown = true
+		this.isLastAnswerCorrect = correctness
+		this.questionsCorrectness.push(correctness)
     this.updateView()
   }
 }
@@ -57,11 +77,16 @@ window.addEventListener('load', function () {
   document.querySelector('#game').onclick = (event) => {
     const classList = event.target.classList
 
-    if(classList.contains('option')) {
-      game.showAnswer()
-    }
-    else if(classList.contains('next')) {
-      game.showNextQuestion()
-    }
+		if (! game.answerIsShown) {
+			if(classList.contains('option-correct')) {
+				game.showAnswer(true)
+			}
+			if(classList.contains('option-wrong')) {
+				game.showAnswer(false)
+			}
+		}
+		else if(classList.contains('next')) {
+			game.showNextQuestion()
+		}
   }
 })
